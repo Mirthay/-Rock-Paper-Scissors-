1055bcd2c8b6bb51b4150a6a777258f58015aa60
Mirthay <mario991@gmail.com>
2021-04-29 23:51:15 +0200

    zmena router

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TheRules from "./components/TheRules.vue";
-import App from "./App.vue";
+import GameItems from "./components/GameItems.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/game" },
-    { path: "/game", component: App },
+    { path: "/game", component: GameItems },
     { path: "/:notFound(.*)", redirect: "/game" },
     { path: "/rules", component: TheRules },
   ],
