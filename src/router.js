6e67ac4efb0f4a3a03c37b30477130fd189458a6
Mirthay <mario991@gmail.com>
2021-05-04 15:09:16 +0200

    vymazane routes

--- src/router.js
+++ src/router.js
@@ -1,13 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import TheRules from "./components/TheRules.vue";
-import GameItems from "./components/GameItems.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", redirect: "/game" },
-    { path: "/game", component: GameItems },
-    { path: "/:notFound(.*)", redirect: "/game" },
+    { path: "/:notFound(.*)", redirect: "/rules" },
     { path: "/rules", component: TheRules },
   ],
 });
